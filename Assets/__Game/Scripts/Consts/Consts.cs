e25ea0e55d6403bdd3707ea49b64deef7d81dc76
thanhdhyk117 <nguyentrungthanh12h@gmail.com>
2025-07-27 12:09:10 +0700

    + add const value

--- Assets/__Game/Scripts/Consts/Consts.cs
+++ Assets/__Game/Scripts/Consts/Consts.cs
@@ -1,18 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Consts : MonoBehaviour
 {
     //Anim trigger value
-    public static readonly int ANIM_IDLE = Animator.StringToHash("IsIdle");
-    public static readonly int ANIM_RUN = Animator.StringToHash("IsRun");
-    public static readonly int ANIM_ATTACK= Animator.StringToHash("IsAttack");
-    public static readonly int ANIM_WIN = Animator.StringToHash("IsWin");
-    public static readonly int ANIM_DANCE = Animator.StringToHash("IsDance");
-    public static readonly int ANIM_ULTI = Animator.StringToHash("IsUlti");
-    public static readonly int ANIM_DEAD = Animator.StringToHash("IsDead");
-    
+    public static readonly string ANIM_IDLE = ("IsIdle");
+    public static readonly string ANIM_RUN = ("IsRun");
+    public static readonly string ANIM_ATTACK = ("IsAttack");
+    public static readonly string ANIM_WIN = ("IsWin");
+    public static readonly string ANIM_DANCE = ("IsDance");
+    public static readonly string ANIM_ULTI = ("IsUlti");
+    public static readonly string ANIM_DEAD = ("IsDead");
+
     //Tag value
     public const string TAG_CHARACTER = "Character";
+}
+
+
+public enum ETargetType
+{
+    None = 0,
+    Nearest = 1,
+    Farthest = 2
 }
